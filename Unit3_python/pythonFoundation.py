59c686fb42c5caaec01f9243215f2c55597752cb
jibraelT <jthomas610@boyslatin.net>
2024-10-31 12:46:41 +0000

    worked on python data types

---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,34 @@
+# data types = are the fundamental building
+#block of Python
+
+##Integers = whole numbera
+# strings = any characters wrapped in quotes
+#boolean = true and false\
+# we output data using the print function
+print(200 * 8)
+print('welcome to python. !@#')
+print(True)
+
+()#round brackets = function
+{}# curly brackets = objects
+[]# square brackets = list
+
+#variables = container for data
+# we write variables by first declaring =/ giving it a name
+# we use the equal sign
+#we then give it data
+message='Good morning'
+name = 'Jibrael'
+
+#concatenation = combining strings
+print( name, message)
+
+storyA= 'I will be going to '
+location ='school'
+time ='at the time of 8:30'
+print(storyA, location, time )
+
+valA=10
+valB=20
+
+print( valA + valB)
