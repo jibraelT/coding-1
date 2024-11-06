1392a78dd403dd6f922dd0708127d5192fdf269e
jibraelT <jthomas610@boyslatin.net>
2024-11-06 13:46:23 +0000

    python review

---
+++ Unit3_python/asyncActivity_nov1.py
@@ -0,0 +1,65 @@
+# keywords
+#print () - round brackets
+# variables
+# datatypes
+
+#clues
+# add two numbers to create 245
+# variables----> put data insidwe of the variables---> print result
+
+a= 200
+b= 45
+print(a + b )
+
+
+' I would use an integer data type for the days of yoe week'
+' I would use the string data type for the string days of the week'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
