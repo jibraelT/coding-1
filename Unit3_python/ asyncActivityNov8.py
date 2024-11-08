b13c426e29b8c20a03a52f67c24c6803cb382f7b
jibraelT <jthomas610@boyslatin.net>
2024-11-08 13:49:31 +0000

    async workl

---
+++ Unit3_python/ asyncActivityNov8.py
@@ -0,0 +1,28 @@
+stringConcentration = ' when two diffrent varibales are put together'
+
+storyA= 'I will be going to '
+location ='school'
+time ='at the time of 8:30'
+print(storyA, location, time )
+
+
+rules = ' we write variables by first declaring =/ giving it a name we then give it data '
+print( rules )
+
+camelCase ='when naming a variable everyword after the first word is capitilized '
+snakeCase = 'when naming a variable, every word after the first word is spaced out with an underscore. '
+pascalCase = ' when naming a variable Every new word is capitilized'
+
+a = '3'
+b = '100'
+
+print( a * b)
+
+
+
+
+
+
+
+
+
