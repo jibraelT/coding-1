7d31d7c84b8a32b589da1d36d2e055386d3ba659
jibraelT <jthomas610@boyslatin.net>
2024-11-19 13:47:14 +0000

    Finished function

---
+++ Unit3_python/functions.py
@@ -0,0 +1,54 @@
+# what is a function?
+
+# A function is a set of instructions taht will perform a specific task
+
+
+# there are two variations of functions: userdefined functions
+# and built in functions.
+
+# Built in function: instructions that have already been written and are
+# built in to the programming language 
+
+print('Jibraels coding class') # print is a built-in function . we can identify functions
+# by the rouynd brackets.
+
+number = input(' please type in a number?') # another example of a built-in function
+
+print(number)
+
+# all data types, variables, and operators are just pieces for us
+# to use in order to write instruction for function
+
+# User Defined Functions - custom function created by an egineer
+
+def pbAndj():
+      print ('step 1. get two slices of bread')
+      print(' step 2. spread peanut butter on one side of bread')
+      print('step 3. spread jelly on the other side of the bread')
+      print('step 4. put sandwhich togther with spreads')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
