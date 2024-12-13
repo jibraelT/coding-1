6fca605892220319e8b5280c8aada777aa1fc871
jibraelT <jthomas610@boyslatin.net>
2024-12-13 13:48:11 +0000

    part 1 is done could no reach part 2

---
+++ Unit3_python/asyncActivityDec13.py
@@ -0,0 +1,18 @@
+def positiveNegative(numbers):
+ if numbers > -2:
+  print('if number is less than 0 it is a negative')
+
+ elif numbers > -1:
+    print('this is a negative number')
+ elif numbers == 0:
+   print('this is 0 ')
+ elif numbers < 1:
+         print('if number is greater than 0 it is a positive ')
+ elif numbers < 2:
+  print('this is a positive ')
+
+positiveNegative(-1)
+positiveNegative(0)
+positiveNegative(1)
+
+
