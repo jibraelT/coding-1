2f0ef58b3ed3a3a61b4e300f35fb6e96a87aef0c
jibraelT <jthomas610@boyslatin.net>
2024-12-18 13:47:05 +0000

    didnt finish

--- Unit3_python/asyncActivityDec13.py
+++ Unit3_python/asyncActivityDec13.py
@@ -1,18 +1,22 @@
 def positiveNegative(numbers):
  if numbers > -2:
   print('if number is less than 0 it is a negative')
-
- elif numbers > -1:
-    print('this is a negative number')
+  
  elif numbers == 0:
    print('this is 0 ')
+ 
  elif numbers < 1:
          print('if number is greater than 0 it is a positive ')
- elif numbers < 2:
-  print('this is a positive ')
+ 
 
-positiveNegative(-1)
+positiveNegative(-2)
 positiveNegative(0)
 positiveNegative(1)
 
 
+
+def shoppingDiscount(membership,itemName,itemPrice):
+ if membership == 'superShoper': 
+    discountAmount = itemPrice =0.1
+    finalAmount = itemPrice -discountAmount
+    
