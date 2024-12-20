eca2946bd71fb00039690225c94247a8cd7d57f5
jibraelT <jthomas610@boyslatin.net>
2024-12-20 08:51:12 +0000

    was sick but still tried

---
+++ Unit3_python/asyncActivityNov20.py
@@ -0,0 +1,37 @@
+def positiveNegative(numbers):
+ if numbers > -2:
+  print('if number is less than 0 it is a negative')
+  
+ elif numbers == 0:
+   print('this is 0 ')
+ 
+ elif numbers < 1:
+         print('if number is greater than 0 it is a positive ')
+ 
+
+positiveNegative(-2)
+positiveNegative(0)
+positiveNegative(1)
+
+
+def shoppingDiscount(membership,itemName,itemPrice):
+ if membership == 'superShoper': 
+    discountAmount = itemPrice =0.1
+    finalAmount = itemPrice -discountAmount
+print('if you have a 10 inch tv the member ship will bring the price down to 9.9')
+      
+
+def moneyAmount(age):
+ if age > 40:
+  print('if age is less than 40 iy is 25.00')
+  
+ elif age == 24:
+   print('if age is 24 it is 15.00 ')
+ 
+ elif age < 18:
+         print(' if age is greater than 18 it is 10.00')
+ 
+
+
+
+
